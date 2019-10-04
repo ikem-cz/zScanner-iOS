e5254810de0d8c70d4bbc8c55cb04650809bb578
Jakub Skorepa <jakub.skorepa@poqstudio.com>
2019-10-04 12:36:31 +0100

    Update API url

--- zScanner/SupportFiles/Helpers/Config.swift
+++ zScanner/SupportFiles/Helpers/Config.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 enum Config {
-    static let productionURL: String = "https://tempra.ikem.seacat/api-zscanner-new/v3"
+    static let productionURL: String = "https://tempra.ikem.seacat/api-zscanner/v3"
     static let testingURL: String = "https://desolate-meadow-62603.herokuapp.com/api-zscanner/v3"
     static let authUrl: String = "http://auth.ikem.seacat"
     
