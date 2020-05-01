e50eb798d3c902d69b6925d0d168756d87f42d56
Jan Provazník <proovaznik@gmail.com>
2020-05-01 17:34:06 +0200

    Update pods

---
+++ zScanner/Views/NewDocument/CameraView.swift
@@ -0,0 +1,21 @@
+//
+//  CameraView.swift
+//  zScanner
+//
+//  Created by Jan Provazník on 01/05/2020.
+//  Copyright © 2020 Institut klinické a experimentální medicíny. All rights reserved.
+//
+
+import UIKit
+
+class CameraView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ zScanner/Views/NewDocument/MediaTypeCollectionViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  CollectionViewCell.swift
+//  zScanner
+//
+//  Created by Jan Provazník on 01/05/2020.
+//  Copyright © 2020 Institut klinické a experimentální medicíny. All rights reserved.
+//
+
+import UIKit
+
+class MediaTypeCollectionViewCell: UICollectionViewCell {
+    // MARK: Interface
+    func setup(with title: String) {
+        titleLabel.text = title
+    }
+    
+    // MARK: Helpers
+    private func setupView() {
+        titleLabel.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+        }
+    }
+    
+    private lazy var titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.font = .body
+        titleLabel.textColor = .white
+        titleLabel.textAlignment = .center
+        return titleLabel
+    }()
+}
